4af54669e1d06a35b20fb2c1345a5f06357a9c91
Евгений Анфалов <anfalovd10@yandex.ru>
2016-11-19 23:38:38 +0500

    fix

---
+++ Week8/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week8
+{
+    class Program
+    {
+        static IEnumerable<string[]> ReadCsv1(string filename)
+        {
+            using (var stream = new StreamReader(filename))
+                while (true)
+                {
+                    if (stream.EndOfStream)
+                    {
+                        stream.Close();
+                        yield break;
+                    }
+                    var str = stream.ReadLine().Split(',');
+                    for (var i = 0; i < str.Length; i++)
+                        if (str[i] == "NA")
+                            str[i] = null;
+                        else str[i] = str[i].Replace("\"", null);
+                    yield return str;
+                }
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (var strAr in ReadCsv1("C:\\Users\\Евгений\\Desktop\\airquality.csv"))
+            {
+                foreach (var str in strAr)
+                {
+                    Console.Write("{0} ", str);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
